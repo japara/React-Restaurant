8e1ff561222f0a66db0b7d3a9b50feea826a1985
japara <72559893+japara@users.noreply.github.com>
2022-09-07 18:58:20 +0400

    contact about us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+import Main from "./Main";
 
 function App() {
   return (
     <>
+      <Main />
       <AboutUs />
+      <Contact />
     </>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Carousel1 from "./Carousel";
+import Footer from "./Footer";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <Carousel1 />
+      <Footer />
+    </>
+  );
+}
